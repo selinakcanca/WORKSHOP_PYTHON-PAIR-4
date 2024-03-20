358525f45abd419a4c916bd5c3358cb4803fdaee
Selin <94780380+selinakcanca@users.noreply.github.com>
2024-03-20 15:24:40 +0300

    Update 1VKI.py
    Update

--- 1VKI.py
+++ 1VKI.py
@@ -1,17 +1,11 @@
 #1-Kullanıcının girdiği boy ve ağırlık değerlerine göre vücut kitle indeksini (VKİ = ağırlık/(boy*boy)) hesaplayınız.
 
-agirlik = float(input("Lütfen ağırlığınızı giriniz."))
-boy = float(input("Lütfen boyunuzu giriniz."))
-vki = agirlik / (boy*boy) #formülümüz
+agirlik = float(input("Lütfen kilonuzu kg cinsinden giriniz: "))
+boy = float(input("Lütfen boyunuzu metre cinsinden giriniz: "))
+vki = agirlik/(boy*boy)
 
-if 0 <= vki <= 18:
- print("Zayıfsınız")  
-elif 18 < vki <=24:
- print("Normalsiniz")
-elif 24 < vki <=29:
- print("Fazla kilolusunuz")
-else:
- print("Vücut kitle indeksiniz çok yüksek, ,ideal kilonun çok üstündesiniz")
+totalText = f"Vücut kitle indeksiniz= {vki}'dir."
+print(totalText)
 
 
 
