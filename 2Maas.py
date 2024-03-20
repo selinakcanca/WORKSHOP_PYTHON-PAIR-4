23173aad2f567af8704ce0ea74e921fe52039f83
Selin <94780380+selinakcanca@users.noreply.github.com>
2024-03-20 15:25:03 +0300

    Update 2Maas.py
    update

--- 2Maas.py
+++ 2Maas.py
@@ -1,23 +1,9 @@
 #2-Maaşı ve zam oranı girilen işçinin zamlı maaşını hesaplayarak ekranda gösteriniz.
 
-#birinci çözüm
-def zamli_maas_hesapla(maas, zam_orani):
-    zam_miktari = maas * (zam_orani / 100)
-    zamli_maas = maas + zam_miktari
-    return zamli_maas
+maas = float(input("Lütfen maaşınızı giriniz: "))
+zamOrani = float(input("Lütfen zam oranınızın yüzde kaç olduğunu giriniz: "))
+zamliMaas = (maas*(100 + zamOrani)/100)
 
-maas = float(input("Maaşı giriniz: "))
-zam_orani = float(input("Zam oranını giriniz (% olarak): "))
-
-zamli_maas = zamli_maas_hesapla(maas, zam_orani)
-print("Zamlı maaş:", zamli_maas)
-
-
-
-#ikinci çözüm
-yeniMaas=0
-maas=input("Maaşı Gir : ")
-zam=input("Zam Oranı(%) : ")
-yeniMaas=float(maas)+(float(maas)*float(zam)/100)
-print("Zamlı Maaş :",yeniMaas)
+totalText = f"Zamlı maaşınız= {zamliMaas}"
+print(totalText)
 
