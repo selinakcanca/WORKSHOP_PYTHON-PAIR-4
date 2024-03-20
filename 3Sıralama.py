8f360d5cbf5f3fa453282d3e3c62cfb2fe2d8c06
Selin <94780380+selinakcanca@users.noreply.github.com>
2024-03-20 15:25:19 +0300

    Update 3Sıralama.py
    update

--- 3Sıralama.py
+++ 3Sıralama.py
@@ -1,17 +1,16 @@
 #3-Kullanıcının girdiği üç sayı arasında en büyük olanı bulan ve sonucu yazdıran bir program yazınız.
 
 
-def en_buyuk_bul(sayi1, sayi2, sayi3):
-    if sayi1 >= sayi2 and sayi1 >= sayi3:
-        return sayi1
-    elif sayi2 >= sayi1 and sayi2 >= sayi3:
-        return sayi2
-    else:
-        return sayi3
+sayi1 = int(input("Bir sayı giriniz: "))
+sayi2 = int(input("İkinci sayıyı giriniz: "))
+sayi3 = int(input("Üçüncü sayıyı giriniz: "))
 
-sayi1 = float(input("Birinci sayıyı giriniz: "))
-sayi2 = float(input("İkinci sayıyı giriniz: "))
-sayi3 = float(input("Üçüncü sayıyı giriniz: "))
+if (sayi1>sayi2) and (sayi1>= sayi3):
+    buyukSayi = sayi1
+elif (sayi2>=sayi1) and (sayi2>=sayi3):
+    buyukSayi=sayi2
+else:
+    buyukSayi=sayi3
 
-en_buyuk = en_buyuk_bul(sayi1, sayi2, sayi3)
-print("En büyük sayı:", en_buyuk)
+totalText = f"Girdiğiniz sayılardan en büyüğü= {buyukSayi}."
+print(totalText)
